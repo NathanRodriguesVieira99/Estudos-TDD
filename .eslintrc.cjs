9d6529ff3c3c2bfe24a1dcf94e1e3507705a5626
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-09-05 22:39:55 -0300

    fix: add typescript parser to ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,10 @@
 module.exports = {
   extends: [],
+  parser: "@typescript-eslint/parser",
   plugins: ["simple-import-sort"],
   rules: {
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
-    '@typescript-eslint/no-explicit-any':'off'
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
