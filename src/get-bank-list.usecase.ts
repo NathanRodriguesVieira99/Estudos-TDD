c79f23b2d8b26450ce4aa3f0d5f3fe58e3c92024
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-09-07 23:30:16 -0300

    refactor(bank): add useCase type contract for GetBankListUseCase

--- src/get-bank-list.usecase.ts
+++ src/get-bank-list.usecase.ts
@@ -1,9 +1,23 @@
 import type { BankDAO } from "./bank.dao-database.ts";
+import type { UseCase } from "./useCase.ts";
 
-export class GetBankListUseCase {
+export namespace GetBankList {
+  export type Input = unknown;
+  export type Output = {
+    id: number;
+    codigo: string;
+    nome: string;
+    url: string;
+  }[];
+}
+
+export class GetBankListUseCase implements UseCase<
+  GetBankList.Input,
+  GetBankList.Output
+> {
   constructor(private readonly bankDAO: BankDAO) {}
 
-  async execute(): Promise<any> {
+  async execute(): Promise<GetBankList.Output> {
     const rows = await this.bankDAO.list();
     const output = rows.map((row) => ({
       id: row.BANCO_ID,
